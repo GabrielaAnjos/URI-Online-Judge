24b6908f61da0df00a411609597fc291cfd5f918
GabrielaAnjos <gabrielalima.anjos@gmail.com>
2021-09-16 23:04:35 -0300

    Atualização 4

---
+++ matematica/URI1161_SomaDeFatoriais.java
@@ -0,0 +1,12 @@
+package urijudge.desafios.matematica;
+
+import java.util.Scanner;
+
+public class URI1161_SomaDeFatoriais {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int meias = input.nextInt();
+        System.out.println(meias + 1);
+    }
+}
